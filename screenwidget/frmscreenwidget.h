6f8d2d45e3ba6551e53d61e5e9bc6427a7e713c4
lixianping <lxp17683722050@163.com>
2023-04-03 18:34:36 +0800

    截图工具

---
+++ screenwidget/frmscreenwidget.h
@@ -0,0 +1,25 @@
+﻿#ifndef FRMSCREENWIDGET_H
+#define FRMSCREENWIDGET_H
+
+#include <QWidget>
+#include <QKeyEvent>
+namespace Ui {
+class frmScreenWidget;
+}
+
+class frmScreenWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit frmScreenWidget(QWidget *parent = 0);
+    ~frmScreenWidget();
+
+private slots:
+    void on_pushButton_clicked();
+    void keyPressEvent(QKeyEvent *event);
+private:
+    Ui::frmScreenWidget *ui;
+};
+
+#endif // FRMSCREENWIDGET_H
